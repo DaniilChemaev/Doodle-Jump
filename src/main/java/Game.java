3efee33d7ac11b17cf23b60a31f727799894a329
Daniil Chemaev <chemaev.ds@gmail.com>
2022-12-04 21:20:44 +0300

    Added players left-right movements

---
+++ src/main/java/Game.java
@@ -0,0 +1,100 @@
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+
+
+public class Game extends Application {
+    Image backgroundImg = new Image("images/background.png");
+    private static final int WIDTH = 450;
+    private static final int HEIGHT = 900;
+
+    private HashMap<KeyCode, Boolean> keys = new HashMap<>();
+
+    private Pane gameRoot;
+    private Pane appRoot;
+
+    public Player player;
+
+    private void newPlayer() {
+        player = new Player();
+        gameRoot.getChildren().add(player);
+    }
+
+    private void initContent() {
+        ImageView background = new ImageView(backgroundImg);
+        background.setFitHeight(HEIGHT);
+        background.setFitWidth(WIDTH);
+        appRoot.getChildren().addAll(background, gameRoot);
+    }
+
+    private void update() {
+        playerControl();
+        checkSide();
+    }
+
+    private void playerControl() {
+        if (player.getTranslateY() >= 5) {
+            player.jump();
+            player.setCanJump(false);
+        }
+        if (isPressed(KeyCode.LEFT)) {
+            player.setScaleX(-1);
+            player.moveX(-7);
+        }
+        if (isPressed(KeyCode.RIGHT)) {
+            player.setScaleX(1);
+            player.moveX(7);
+        }
+        if (player.playerVelocity.getY() < 10) {
+            player.playerVelocity = player.playerVelocity.add(0, 1);
+        } else player.setCanJump(false);
+        player.moveY((int) player.playerVelocity.getY());
+    }
+
+    private void checkSide() {
+        System.out.println(player.getTranslateX());
+        if (player.getTranslateX() < -72) {  // Если ушел влево
+            player.setTranslateX(430);
+        } else if (player.getTranslateX() > 430) {  // Если ушел вправо
+            player.setTranslateX(-60);
+        }
+    }
+
+    private boolean isPressed(KeyCode key) {
+        return keys.getOrDefault(key, false);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        appRoot = new Pane();
+        gameRoot = new Pane();
+        appRoot.setPrefSize(WIDTH, HEIGHT);
+
+        initContent();
+        newPlayer();
+
+        Scene scene = new Scene(appRoot);
+        scene.setOnKeyPressed(event -> keys.put(event.getCode(), true));
+        scene.setOnKeyReleased(event -> keys.put(event.getCode(), false));
+
+        primaryStage.setTitle("Doodle Jump");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+            }
+        };
+        timer.start();
+    }
+}
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,4 @@
+import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -5,13 +6,41 @@ import javafx.scene.layout.Pane;
 public class Player extends Pane {
     private static final double WIDTH = 76;
     private static final double HEIGHT = 76;
+    public Point2D playerVelocity = new Point2D(0, 0);
+
+    private boolean canJump;
     private static final Image playerImg = new Image("images/doodler.png");
-    private static final ImageView imageView = new ImageView(playerImg);
+    private static final ImageView playerView = new ImageView(playerImg);
 
     public Player() {
-        imageView.setFitHeight(HEIGHT);
-        imageView.setFitWidth(WIDTH);
+        playerView.setFitHeight(HEIGHT);
+        playerView.setFitWidth(WIDTH);
+        this.canJump = true;
 
-        getChildren().addAll(imageView);
+        getChildren().addAll(playerView);
     }
+
+    public void moveX(int value) {
+        boolean movingRight = value > 0;
+
+        for (int i = 0; i < Math.abs(value); i++) {
+            this.setTranslateX(this.getTranslateX() + (movingRight ? 1 : -1));
+        }
+    }
+
+    public void moveY(int value) {
+    }
+
+    public void jump() {
+        if (canJump) {
+            playerVelocity = playerVelocity.add(0, -30);
+            canJump = false;
+        }
+    }
+
+    public void setCanJump(boolean canJump) {
+        this.canJump = canJump;
+    }
+
+
 }
